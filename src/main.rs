596111c1dee2c565920ac5a1a908590db954c620
darksv <darek969-12@o2.pl>
2023-08-19 16:43:53 +0200

    refactor: simplify updating ui in `clock_ticker` task

--- src/main.rs
+++ src/main.rs
@@ -164,12 +164,8 @@ async fn ui_update_loop(window: alloc::rc::Rc<MinimalSoftwareWindow>, mut displa
 #[embassy_executor::task]
 async fn clock_ticker(ui: MainWindow) {
     let mut ticker = Ticker::every(Duration::from_secs(1));
-    let ui_weak = ui.as_weak();
     loop {
-        let ui = ui_weak.upgrade().unwrap();
-        let mut clock = ui.as_weak().upgrade().unwrap().get_clock();
-        clock += 1;
-        ui.set_clock(clock);
+        ui.set_clock(ui.get_clock() + 1);
         //println!("Secs:{}", clock);
         ticker.next().await;
     }
